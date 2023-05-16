ef7fdc0601aa8c324a9477e4a2315f46164f8346
Eman19194 <emyem19194@gmail.com>
2023-05-16 11:21:11 +0300

    c

--- phase2/resources/views/index.blade.php
+++ phase2/resources/views/index.blade.php
@@ -112,6 +112,7 @@
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
           </script>
           <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+          <script src="{{ asset('js/validation.js') }}"></script>
 
           <script>
             $(document).ready(function(){
